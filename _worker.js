67c15b2f86cddaac1e3e1ae1067824eabcd00239
lym1979424-proton <165483953+lym1979424-proton@users.noreply.github.com>
2024-10-04 16:50:11 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'c0a27a6f-e932-4c49-81f5-587e354d0b66';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
